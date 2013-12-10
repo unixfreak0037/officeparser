f56d76d713a50c1016f19e378f2687f71190880b
John Davison <unixfreak0037@gmail.com>
2013-12-10 10:58:22 -0500

    last pull request broke script

--- officeparser.py
+++ officeparser.py
@@ -124,14 +124,14 @@ class CompoundBinaryFile:
 
         # if the file is a zipfile, extract the binary part to a tempfile and continue,
         # otherwise, proceed as if a real binary file.
-        if zipfile.is_zipfile(self.file):
-            zfile = zipfile.ZipFile(self.file, "r")           
-            data = zfile.read(BINFILE_PATH)             
-            self.f = tempfile.TemporaryFile()           
-            self.f.write(data)
-            self.f.seek(0)                              # rewind the data file to the beginning
-        else:
-            self.f = open(self.file, 'rb')
+        #if zipfile.is_zipfile(self.file):
+            #zfile = zipfile.ZipFile(self.file, "r")           
+            #data = zfile.read(BINFILE_PATH)             
+            #self.f = tempfile.TemporaryFile()           
+            #self.f.write(data)
+            #self.f.seek(0)                              # rewind the data file to the beginning
+        #else:
+        self.f = open(self.file, 'rb')
 
         if parser_options.fail_on_invalid_sig:
             sig = self.f.read(8)
